8b94f0b1a672b1ceec39e09b36ef21b4d0c8802e
Bechara <80393521+m3allimm4a4@users.noreply.github.com>
2023-04-29 17:01:19 +0300

    add angular service worker

--- movies-db-ui/src/app/app.module.ts
+++ movies-db-ui/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { appInitializeFactory } from './app-initialize-factory';
@@ -8,10 +8,16 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppConfigService } from './services/app-config-service/app-config.service';
 import { HttpAuthInterceptor } from './interceptors/http-auth/http-auth.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     {
       provide: APP_INITIALIZER,
