1a5b5d35f368694f1f79a0cdd31b09992b420884
Bechara <80393521+m3allimm4a4@users.noreply.github.com>
2023-04-29 18:37:25 +0300

    add spring boot api

---
+++ movies-db-api/src/main/java/com/beshara/moviesdbapi/MoviesdbapiApplication.java
@@ -0,0 +1,13 @@
+package com.beshara.moviesdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesdbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesdbapiApplication.class, args);
+	}
+
+}
